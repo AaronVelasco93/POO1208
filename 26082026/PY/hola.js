8b9ff64848d57b674bb6b7a4c9b336ed57336619
AaronVelasco93 <aaronvelasco1993@outlook.com>
2026-08-26 11:19:57 -0600

    clase de 26082026

---
+++ 26082026/PY/hola.js
@@ -0,0 +1,13 @@
+// let nombre = "Aaron";
+// console.log("Hola " + nombre + "!");
+
+// let edad = parseInt(prompt("¿Cuántos años tienes?"));
+// if (edad < 18) {
+//     console.log("Eres menor de edad.");
+// }
+
+
+function suma(numero1,numero2) {
+    let resultado = numero1 + numero2;
+    return resultado;
+}
---
+++ 26082026/PY/hola.py
@@ -0,0 +1,6 @@
+nombre = "Aaron";
+print("Hola " + nombre + "!")
+
+edad = int(input("¿Cuántos años tienes? "))
+if edad < 18:
+    print("Eres menor de edad.")        
